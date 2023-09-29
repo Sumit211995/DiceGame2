492de55059e1f57ad00eb55aac3b3edec9066ddb
Sumit211995 <sumitcskaktwan@gmail.com>
2023-09-29 13:32:31 +0530

    Adding more functionality with js

--- script.js
+++ script.js
@@ -12,20 +12,42 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 
-
+let scores,currentScore,activePlayer,playing;
 
 //Setting Starting part
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+const init = function(){
 
-let currentScore = 0;
-let activePlayer = 0;
+    scores = [0,0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+};
+init();
+
+
+const switchPlayer = function() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
+}
 
 
 //Roll Dice
 btnRoll.addEventListener("click", function(){
+    if(playing){
     const dice = Math.floor(Math.random()*6)+1; 
     //console.log(dice);
 
@@ -41,12 +63,30 @@ btnRoll.addEventListener("click", function(){
         //current0El.textContent = currentScore;
     }else{
         //switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle("player--active");
-        player1El.classList.toggle("player--active");
-        
+        switchPlayer();
     }
-        
+}
 });
+
+btnHold.addEventListener("click", function(){
+    if(playing){
+    //add current score to active player score
+    scores[activePlayer] += currentScore;
+    //score[1] = score[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    //check if score is >=100
+    if(scores[activePlayer] >= 100){
+        //finish the game
+        playing = false;
+        diceEl.classList.add("hidden");
+        document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+        document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+    }else{
+        //switch to the next player
+        switchPlayer();
+    }
+}
+});
+
+btnNew.addEventListener("click",init);
